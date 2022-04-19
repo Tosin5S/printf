96d63c4a29586c4ffda392b74d182ea6a7c0b666
Tosin5S <olyguy.aq@gmail.com>
2022-04-19 04:49:05 -0700

    handling conversion specifiers of 'c', 's', '%', 'd' and 'i'

---
+++ main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions.
+ */
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
+int _printf(const char *format, ...);
+int print_buf(char *buf, unsigned int nbuf);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int print_chr(va_list arguments, char *buf, unsigned int ibuf);
+int print_int(va_list arguments, char *buf, unsigned int ibuf);
+int print_prg(va_list a __attribute__((unused)), char *buf, unsigned int i);
+int print_str(va_list arguments, char *buf, unsigned int ibuf);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int ev_print_func(const char *s, int index);
+
+#endif
